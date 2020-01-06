653cbe8b070f29798b3b98afcc9d528245ecd851
Azin Asili <azinasili@gmail.com>
2020-01-06 16:43:15 -0500

    fix linting issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,32 @@
 // _document is only rendered on the server side and not on the client side
 // Event handlers like onClick can't be added to this file
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import React from 'react';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
 
 class AzinAsiliDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
 
-  render() {
+  render(): React.ReactElement {
     return (
       <Html>
         <Head>
-          <link rel="stylesheet" type="text/css" href="https://unpkg.com/normalize.css" />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://unpkg.com/normalize.css"
+          />
           <style jsx global>
             {`
               html {
