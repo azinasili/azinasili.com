6d83581dd9188586f4f0d3caae095e3fe9eeb005
Azin Asili <hello@azinasili.com>
2022-03-05 17:48:02 -0500

    add some content

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -3,9 +3,32 @@ import type { NextPage } from 'next';
 
 const ResumePage: NextPage = () => (
   <section>
-    <p>
-      I&apos;m a super important resume.
-    </p>
+    <h1>Links</h1>
+    <ul>
+      <li>Github</li>
+      <li>Codepen</li>
+      <li>Linkedin</li>
+      <li>...</li>
+    </ul>
+    <h2>Skills</h2>
+    <ul>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>Next</li>
+      <li>...</li>
+    </ul>
+    <h2>Experience</h2>
+    <h3>ShopRunner</h3>
+    <p>...</p>
+    <h3>Graphite GTC</h3>
+    <p>...</p>
+    <h3>GSW</h3>
+    <p>...</p>
+    <h3>AG Marketing Company</h3>
+    <p>...</p>
+    <h2>Education</h2>
+    <h3>Drexel University</h3>
+    <p>...</p>
   </section>
 );
 
