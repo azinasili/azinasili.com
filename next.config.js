eab141b575f90cd979fdb2dd9ffca03745af743c
Azin Asili <azinasili@gmail.com>
2022-04-30 19:48:42 -0400

    added stylec-components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactRemoveProperties: true,
   reactStrictMode: true,
+  removeConsole: process.env.NODE_ENV === 'production',
+  styledComponents: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
