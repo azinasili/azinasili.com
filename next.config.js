165a6f9d9d6752dfc3ae43e3c9634aada3e929ec
Azin Asili <azinasili@gmail.com>
2022-04-30 21:53:03 -0400

    add styled-components settings

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactRemoveProperties: true,
+  compiler: {
+    reactRemoveProperties: true,
+    removeConsole: process.env.NODE_ENV === 'production',
+    styledComponents: true,
+  },
   reactStrictMode: true,
-  removeConsole: process.env.NODE_ENV === 'production',
-  styledComponents: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      process.env.NODE_ENV === 'production' &&
+      {
+        source: '/resume',
+        destination: '/',
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
