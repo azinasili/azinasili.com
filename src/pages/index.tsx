ad578887083ab344ec0e2431311cb6d8cc6d0a6f
Azin Asili <azinasili@gmail.com>
2022-05-02 09:28:25 -0400

    Touchup (#24)
    * created a global style
    
    * use best practices
    
    * use best practices
    
    * fix lints

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import type { Project } from 'types/Project';
 import type { ProfileLink } from 'types/Profile';
 import Basic from 'layouts/Basic';
@@ -12,21 +12,23 @@ interface HomePageProps {
   profileLinks: ProfileLink[];
 }
 
+export const getServerSideProps: GetServerSideProps<HomePageProps> = async () => {
+  const profileLinks = await getProfileLinks();
+  const projects = await getProjects();
+  return {
+    props: { profileLinks, projects },
+  };
+}
+
 const Home: NextPage<HomePageProps> = ({ profileLinks, projects }) => (
   <Basic title="Home" description="my home page" profileLinks={profileLinks}>
     <p>
       I&apos;m a software engineer with a passion for building for the web. I
       rely heavily on my design background to create beautiful experiences. Besides
-      development I enjoy craft beer and talking tech.
+      development, I enjoy craft beer and talking tech.
     </p>
     {process.env.NODE_ENV !== 'production' && <ProjectList projects={projects} />}
   </Basic>
 );
-
-Home.getInitialProps = async (): Promise<HomePageProps> => {
-  const profileLinks = await getProfileLinks();
-  const projects = await getProjects();
-  return { profileLinks, projects };
-};
 
 export default Home;
