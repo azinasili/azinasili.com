211f55a17a0862ff46d56d5aab734e5e80ca4496
Azin Asili <azinasili@gmail.com>
2022-05-02 13:56:51 -0400

    make profile links into a component

---
+++ src/components/ProfileLinks/ProfileLinks.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import type { ProfileLink } from 'types/Profile';
+import ProfileLinkItem from './ProfileLinkItem';
+
+interface ProfileLinksProps {
+  profileLinks: ProfileLink[];
+};
+
+const Items = styled.ul`
+  list-style: none;
+  padding-left: 0;
+`;
+
+const ProfileLinks: React.FC<ProfileLinksProps> = ({ profileLinks }) => {
+  return (
+    <Items>{profileLinks.map(profileLink => <ProfileLinkItem {...profileLink} />)}</Items>
+  );
+}
+
+export default ProfileLinks;
