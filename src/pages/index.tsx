e5c84bd5ee3185e2aa290d89c5c6dff2a8f68b8d
Azin Asili <azinasili@gmail.com>
2022-05-03 01:08:26 -0400

    Simplified homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import type { Project } from 'types/Project';
 import type { ProfileLink } from 'types/Profile';
 import getProjects from 'server/getProjects';
 import getProfileLinks from 'server/getProfileLinks';
-import Footer from 'components/Footer';
+import Basic from 'layouts/Basic';
 import ProfileLinks from 'components/ProfileLinks';
 
 interface HomePageProps {
@@ -20,24 +20,18 @@ export const getServerSideProps: GetServerSideProps<HomePageProps> = async () =>
   };
 }
 
+const Header: React.FC = () => <h1>👋 Hi</h1>;
+
 const Home: NextPage<HomePageProps> = ({ profileLinks, projects }) => (
-  <>
-    <header>
-      <h1>Hi</h1>
-    </header>
-    <main>
-      <p>
-        I&apos;m a software engineer with a passion for building for the web. I
-        rely heavily on my design background to create beautiful experiences. Besides
-        development, I enjoy craft beer and talking tech.
-      </p>
-      <h2>Find me</h2>
-      <ProfileLinks profileLinks={profileLinks} />
-      <h2>Some Projects</h2>
-      <p>...</p>
-    </main>
-    <Footer />
-  </>
+  <Basic title="foo" description="bar" headerSlot={<Header />}>
+    <p>
+      I&apos;m a software engineer with a passion for building for the web. I rely heavily on my design background to create beautiful experiences. Currently I am a <a href="https://www.shoprunner.com/" rel="noreferrer noopener">lead engineer at ShopRunner</a>. Besides development, I enjoy craft beer and talking tech.
+    </p>
+    <h2>🔎 Find me</h2>
+    <ProfileLinks profileLinks={profileLinks} />
+    <h2>🚧 Some Projects</h2>
+    <p>...</p>
+  </Basic>
 );
 
 export default Home;
