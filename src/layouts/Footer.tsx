1001fce3aa4baccca254af5c933bbdac54188e77
Azin Asili <azinasili@gmail.com>
2022-05-03 01:08:57 -0400

    moved footer

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import { em } from 'polished';
+
+const StyledFooter = styled.footer`
+  font-size: ${em(12)};
+  margin-bottom: 0;
+`;
+
+const Footer: React.FC = () => (
+  <StyledFooter>
+    <p>&copy;{new Date().getFullYear()} Azin Asili. All Rights Reserved.</p>
+  </StyledFooter>
+);
+
+export default Footer;
