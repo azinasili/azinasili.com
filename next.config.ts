d1569015d8acffb8dee4627bdf75ee4e417d7f7f
Azin Asili <azin@Asili-MacBook-Pro.local>
2026-01-09 15:25:48 -0500

    modernize deps and configs

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   compiler: {
     reactRemoveProperties: true,
     removeConsole: process.env.NODE_ENV === 'production',
@@ -20,4 +21,4 @@ const nextConfig = {
   }),
 };
 
-module.exports = nextConfig;
+export default nextConfig;
