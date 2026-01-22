2c197efbb55f79314cee2c26cc9cec350406f9fc
Azin <hello@azinasili.com>
2026-01-22 02:45:46 -0500

    refine hook

--- src/hooks/useMouseGradient.tsx
+++ src/hooks/useMouseGradient.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { type RefCallback, useCallback, useEffect, useState } from 'react';
 
 /**
  * Updates CSS variables based on mouse position.
@@ -7,53 +7,63 @@ import { useEffect } from 'react';
  * --mouse-x-pct: horizontal position as a decimal (0 to 1)
  * --mouse-y-pct: vertical position as a decimal (0 to 1)
  */
+function handleMouseTracking(root: HTMLElement) {
+  // Viewport dimensions
+  const width = root.clientWidth;
+  const height = root.clientHeight;
+
+  return function eventHandler(event: MouseEvent) {
+    // Current mouse coordinates
+    const x = event.clientX;
+    const y = event.clientY;
+    const xPct = event.clientX / width;
+    const yPct = event.clientY / height;
+    // Map X position to a Hue (0-360)
+    // Map Y position to a different Hue (0-360)
+    // const hue1 = Math.floor(xPct * 360);
+    const hue1 = Math.floor(xPct * 60 + 180);
+    // const hue2 = Math.floor(yPct * 360);
+    const hue2 = Math.floor(yPct * 60 + 180);
+    // const hue2 = (hue1 + 180) % 360;
+
+    // Update pixel values
+    root.style.setProperty('--mouse-x', `${x}px`);
+    root.style.setProperty('--mouse-y', `${y}px`);
+
+    // Update percentage values (useful for transforms or gradients)
+    root.style.setProperty('--mouse-x-pct', xPct.toFixed(3));
+    root.style.setProperty('--mouse-y-pct', yPct.toFixed(3));
+
+    root.style.setProperty('--hue-1', hue1.toString());
+    root.style.setProperty('--hue-2', hue2.toString());
+  }
+}
+
 export function useMouseGradient() {
+  const [element, setElement] = useState<HTMLElement | null>(null);
+  const ref = useCallback<RefCallback<HTMLElement>>((node) => {
+    if (node !== null) {
+      setElement(node);
+    }
+
+    return () => {
+      setElement(null)
+    }
+  }, []);
+
   useEffect(() => {
     if (typeof window === 'undefined') {
       return;
     }
 
-    const root = document.documentElement;
+    const root = element || document.documentElement;
 
-    if (!root) {
-      return;
-    }
-
-    function handleMouseTracking(event: MouseEvent) {
-      // Viewport dimensions
-      const width = window.innerWidth;
-      const height = window.innerHeight;
-
-      // Current mouse coordinates
-      const x = event.clientX;
-      const y = event.clientY;
-      const xPct = event.clientX / width;
-      const yPct = event.clientY / height;
-
-      // Map X position to a Hue (0-360)
-      // Map Y position to a different Hue (0-360)
-      // const hue1 = Math.floor(xPct * 360);
-      const hue1 = Math.floor(xPct * 60 + 180);
-      // const hue2 = Math.floor(yPct * 360);
-      const hue2 = Math.floor(yPct * 60 + 180);
-      // const hue2 = (hue1 + 180) % 360;
-
-      // Update pixel values
-      root.style.setProperty('--mouse-x', `${x}px`);
-      root.style.setProperty('--mouse-y', `${y}px`);
-
-      // Update percentage values (useful for transforms or gradients)
-      root.style.setProperty('--mouse-x-pct', xPct.toFixed(3));
-      root.style.setProperty('--mouse-y-pct', yPct.toFixed(3));
-
-      root.style.setProperty('--hue-1', hue1.toString());
-      root.style.setProperty('--hue-2', hue2.toString());
-    }
-
-    window.addEventListener('mousemove', handleMouseTracking);
+    window.addEventListener('mousemove', handleMouseTracking(root));
 
     return () => {
-      window.removeEventListener('mousemove', handleMouseTracking);
+      window.removeEventListener('mousemove', handleMouseTracking(root));
     }
-  }, []);
+  }, [element]);
+
+  return [ref];
 }
