239bed963575c9f074af1de1b26450252ddefbc4
Azin <hello@azinasili.com>
2026-01-22 03:12:36 -0500

    handle touches

--- src/hooks/useMouseGradient.tsx
+++ src/hooks/useMouseGradient.tsx
@@ -1,6 +1,8 @@
 import { type RefCallback, useCallback, useEffect, useState } from 'react';
 
-type HandleMouseTrackingReturn = (event: MouseEvent) => void;
+type TrackingEvent = MouseEvent | TouchEvent;
+
+type HandleMouseTrackingReturn = (event: TrackingEvent) => void;
 
 type UseMouseGradientReturn = [RefCallback<HTMLElement>];
 
@@ -12,12 +14,23 @@ type UseMouseGradientReturn = [RefCallback<HTMLElement>];
  * --mouse-y-pct: vertical position as a decimal (0 to 1)
  */
 function handleMouseTracking(root: HTMLElement): HandleMouseTrackingReturn {
+  let point: MouseEvent | Touch;
   const rect = root.getBoundingClientRect();
 
-  return function eventHandler(event) {
+  return function eventHandler(event: TrackingEvent) {
+    if ('touches' in event) {
+      point = event.touches[0];
+      // Note: touchmove can sometimes have empty touches if the finger is lifted
+      if (!point) {
+        return;
+      }
+    } else {
+      point = event;
+    }
+
     // Current mouse coordinates
-    const x = event.clientX - rect.left;
-    const y = event.clientX - rect.top;
+    const x = point.clientX - rect.left;
+    const y = point.clientX - rect.top;
     const xPct = Math.max(0, Math.min(1, x / rect.width));
     const yPct = Math.max(0, Math.min(1, y / rect.height));
     // Map X position to a Hue (0-360)
@@ -46,14 +59,14 @@ let isListening = false;
 export function useMouseGradient(): UseMouseGradientReturn {
   const [element, setElement] = useState<HTMLElement | null>(null);
   const ref = useCallback<RefCallback<HTMLElement>>((node) => {
-    if (node !== null) {
+    if (node !== null && !element) {
       setElement(node);
     }
 
     return () => {
       setElement(null)
     }
-  }, []);
+  }, [element]);
 
   useEffect(() => {
     if (typeof window === 'undefined') {
@@ -64,11 +77,13 @@ export function useMouseGradient(): UseMouseGradientReturn {
 
     if (!isListening) {
       window.addEventListener('mousemove', handleMouseTracking(root));
+      window.addEventListener('touchmove', handleMouseTracking(root), { passive: false });
       isListening = true;
     }
 
     return () => {
       window.removeEventListener('mousemove', handleMouseTracking(root));
+      window.removeEventListener('touchmove', handleMouseTracking(root));
       isListening = false;
     }
   }, [element]);
