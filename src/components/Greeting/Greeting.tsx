f2d4ddea6b6751332814d88f5050d955c02ff4f5
Azin <hello@azinasili.com>
2026-01-22 04:14:12 -0500

    use css modules

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -1,15 +1,5 @@
-import styled from 'styled-components';
-
-const GradientText = styled.span`
-  background: linear-gradient(orange, tomato);
-  background: linear-gradient(135deg, hsl(var(--hue-1), 70%, 60%), hsl(var(--hue-2), 70%, 60%));
-  background-clip: text;
-  // This is not being auto-prefixed for some reason
-  -webkit-background-clip: text;
-  -moz-text-fill-color: transparent;
-  -webkit-text-fill-color: transparent;
-`;
+import styles from './Greeting.module.css';
 
 export function Greeting() {
-  return <h1>👋 <GradientText>Hi</GradientText></h1>;
+  return <h1>👋 <span className={styles.gradientText}>Hi</span></h1>;
 }
