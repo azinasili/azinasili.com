399a1e6fa3363e6c9f4fc6c2619c809cf1f94e73
Azin <hello@azinasili.com>
2026-01-23 12:47:18 -0500

    added prettier

---
+++ prettier.config.ts
@@ -0,0 +1,20 @@
+/**
+ * Prettier will inherit settings from `.editorconfig` file. To reduce configuration
+ * duplication please check there for code style settings.
+ * @see https://prettier.io/docs/configuration
+ */
+const config = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+};
+
+export default config;
