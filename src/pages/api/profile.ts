c29448f755ece20e2c1289eeebe2787ba1cd6674
Azin Asili <hello@azinasili.com>
2026-01-26 02:31:17 -0500

    Remove api (#40)
    * undo
    
    * move file to tmp dir
    
    * remove api
    
    * bump version

--- src/pages/api/profile.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { getProfileLinks } from 'server/getProfileLinks';
-
-export default async function handleProfile(_: NextApiRequest, res: NextApiResponse): Promise<void> {
-  res.status(200).json(getProfileLinks());
-}
