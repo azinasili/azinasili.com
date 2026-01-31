d2be99e513442497f9dacb05c8a810eaa8adf0d0
Azin <hello@azinasili.com>
2026-01-31 14:47:14 -0500

    move styles

--- src/components/PointerList/BasePointerList.tsx
+++ src/components/PointerList/BasePointerList.tsx
@@ -1,7 +1,12 @@
 import type { ComponentPropsWithRef } from 'react';
+import styles from './BasePointerList.module.css';
 
 type BasePointerListProps = ComponentPropsWithRef<'ul'>;
 
 export function BasePointerList({ children, ...props }: BasePointerListProps) {
-  return <ul {...props}>{children}</ul>;
+  return (
+    <ul {...props} className={styles.list}>
+      {children}
+    </ul>
+  );
 }
