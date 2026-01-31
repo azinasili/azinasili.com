96175a5973722cdc156aa5ecadc9c462a5d0fa6a
Azin <hello@azinasili.com>
2026-01-31 15:08:39 -0500

    added stylelint and configs

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,5 +1,6 @@
 const config = {
   '**/*.{js,jsx,ts,tsx,json}': ['npm run prettier:staged', 'npm run eslint:staged'],
+  '**/*.{css}': ['npm run stylelint:staged'],
 };
 
 export default config;
---
+++ stylelint.config.ts
@@ -0,0 +1,5 @@
+import type { Config } from 'stylelint';
+
+export default {
+  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+} satisfies Config;
