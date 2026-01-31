86a95c00ab7f85f0aeca1810bd6b2a6ddbec9536
Azin <hello@azinasili.com>
2026-01-31 15:10:59 -0500

    only allow kebob and camel case

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -2,4 +2,12 @@ import type { Config } from 'stylelint';
 
 export default {
   extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  rules: {
+    'selector-class-pattern': [
+      '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$|^[a-z][a-zA-Z0-9]+$',
+      {
+        message: 'Expected class name to be kebab-case or camelCase',
+      },
+    ],
+  },
 } satisfies Config;
