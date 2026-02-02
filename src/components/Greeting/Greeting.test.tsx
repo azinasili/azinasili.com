9c5df4796cab524abbb2c1665175dfa93d4aa474
Azin Asili <hello@azinasili.com>
2026-02-02 13:21:41 -0500

    Adding test framework (#48)
    * adding test deps
    
    * test setup
    
    * added prepush

---
+++ src/components/Greeting/Greeting.test.tsx
@@ -0,0 +1,10 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { Greeting } from './Greeting';
+
+describe('Greeting', () => {
+  it('renders greeting', () => {
+    render(<Greeting />);
+    expect(screen.getByRole('heading', { level: 1, name: /hi/gi })).toBeInTheDocument();
+  });
+});
