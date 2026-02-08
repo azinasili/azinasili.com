201a32f196d9f424641fdb0847115681b1a6a400
Azin <hello@azinasili.com>
2026-02-08 02:03:04 -0500

    componentized fonts

--- src/hooks/useAddClassToRootNode.tsx
+++ src/hooks/useAddClassToRootNode.tsx
@@ -1,13 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export function useAddClassToRootNode(className: string | Array<string>): void {
+  const classNameRef = useRef<string | Array<string>>('');
+
+  useEffect(() => {
+    classNameRef.current = Array.isArray(className) ? className.join(' ') : className;
+  }, [className]);
+
   useEffect(() => {
     if (typeof window === 'undefined') {
       return;
     }
 
-    const isClassNameArray = Array.isArray(className);
-    const classNamesToAdd = isClassNameArray ? className : [className];
+    const currentClassName = classNameRef.current;
+    const classNamesToAdd = Array.isArray(currentClassName) ? currentClassName : [currentClassName];
 
     classNamesToAdd.forEach(name => document.documentElement.classList.add(name));
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
+import React from 'react';
 import type { AppProps } from 'next/app';
-import { useAddClassToRootNode } from '~/hooks/useAddClassToRootNode';
 import { useMouseTracking } from '~/hooks/useMouseTracking';
-import { azeretMono } from '~/shared/fonts';
+import { Typeface } from '~/components/Typeface';
 import 'modern-normalize/modern-normalize.css';
 import '~/styles/global.css';
 import '~/styles/App.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  useAddClassToRootNode(azeretMono.variable);
   useMouseTracking();
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Typeface />
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
